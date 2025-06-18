1695974a26d12830fce667cec67963b445891cd2
michal <m0556722054@gmail.com>
2025-06-18 13:54:50 +0300

    Time: 13 ms (24.11%), Space: 12.4 MB (80.64%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,26 +1,16 @@
 class Solution(object):
     
-    def map_to_pairs(self, s):
-        pairs = []
-        count = 1
-        for i in range(1, len(s)):
-            if s[i] == s[i - 1]:
-                count += 1
-            else:
-                pairs.append([int(s[i - 1]), count])
-                count = 1
-        pairs.append([int(s[-1]), count])
-        return pairs
-
-    def pairs_to_string(self, pairs):
-        result = ""
-        for digit, count in pairs:
-            result += str(count) + str(digit)
-        return result
-
-    def countAndSay(self, n):
+    def countAndSay(self,n):
         result = "1"
         for _ in range(n - 1):
-            pairs = self.map_to_pairs(result)      # כאן השתנה
-            result = self.pairs_to_string(pairs)   # כאן השתנה
+            prev = result
+            result = ""
+            count = 1
+            for i in range(1, len(prev)):
+                if prev[i] == prev[i - 1]:
+                    count += 1
+                else:
+                    result += str(count) + prev[i - 1]
+                    count = 1
+            result += str(count) + prev[-1]
         return result
